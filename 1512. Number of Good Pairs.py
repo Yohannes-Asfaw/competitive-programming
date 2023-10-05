e3860e4ef5f77875f197f4d6b1ce3d75f98aa293
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2023-10-05 20:01:27 +0300

    Create 1512. Number of Good Pairs.py

---
+++ 1512. Number of Good Pairs.py
@@ -0,0 +1,7 @@
+def numIdenticalPairs(self, nums: List[int]) -> int:
+        ans=0
+        count=Counter(nums)
+        for i in count.keys():
+            if count[i]>1:
+                ans+=(count[i]*(count[i]-1))//2
+        return ans
