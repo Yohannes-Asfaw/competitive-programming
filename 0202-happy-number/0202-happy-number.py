76be41572e5e9661f9cbda8d4624a2c21347035a
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2023-10-21 19:15:43 +0300

    Time: 35 ms (91.14%), Space: 16.1 MB (78.41%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        while n != 1 and n != 4:
+            sum = 0
+            for i in str(n):
+                sum += int(i)**2
+            n = sum
+        return n==1
