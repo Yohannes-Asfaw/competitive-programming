8d1db41d325d4f60cb3219ee7b6a3281b0fc3e76
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2023-10-25 19:03:26 +0300

    Time: 42 ms (89.94%), Space: 18.3 MB (14.92%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=list(s)
+        for i in range(len(s)):
+            if not s[i].isalnum():
+                s[i]=""
+            elif s[i].isalnum() and s[i].isupper():
+                s[i]=s[i].lower()
+        
+        s="".join(s)
+        print(s)
+
+        return s==s[::-1]
+
+
+        
