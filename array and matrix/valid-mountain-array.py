2404035d707bb6f54ab31adc4e3eb726189ee422
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2023-12-27 16:22:25 +0300

    Added solution for Valid Mountain Array

---
+++ array and matrix/valid-mountain-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr)<=2:
+            return False
+        for i in range(len(arr)-1):
+            if arr[i]==arr[i+1]:
+                return False
+                  
+
+        index=0
+
+        for i in range(len(arr)-1):
+            if arr[i]>arr[i+1]:
+                index=i
+                break
+        print(index)
+        if index==0:
+            return False
+        elif arr[:index+1]==sorted(arr[:index+1]) and arr[index+1:]==sorted(arr[index+1:],reverse=True):
+            return True
+        else:
+            return False
+        
+        
