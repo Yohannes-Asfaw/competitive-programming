63fa2b2e0836ed5646faafdd79f0819a32d4e9ae
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2023-12-29 17:17:06 +0300

    Added solution for Largest Number

---
+++ array and matrix/largest-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        ans=[]
+        for i in nums:
+            ans.append(str(i))
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if ans[i]+ans[j]>ans[j]+ans[i]:
+                    continue
+                else:
+                    ans[i],ans[j]=ans[j],ans[i]
+        
+        res="".join(ans)
+        if int(res)==0:
+            return "0"
+        return res
+            
+
+                
+
+        
