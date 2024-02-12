99e5a81be954778d63ba617d6bc885abd4d36a0e
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2024-02-12 14:42:32 +0300

    Add solution for Middle of the Linked List

---
+++ two-pointers and sliding-windows/leetcode/middle-of-the-linked-list.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        first,second=head,head
+        while second and second.next:
+            first=first.next
+            second=second.next.next
+        return first
+
+        
