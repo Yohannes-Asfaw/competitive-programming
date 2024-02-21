5752bb577c9f9aad2713359f16b7165c273f4a53
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2024-02-21 17:16:44 +0300

    Add solution for Score of Parentheses

---
+++ two-pointers and sliding-windows/leetcode/score-of-parentheses.py
@@ -0,0 +1,10 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack=[0]
+        for i in s:
+            if i=="(":
+                stack.append(0)
+            else:
+                value=max(2*stack.pop(),1)
+                stack[-1]+=value
+        return stack[-1]
