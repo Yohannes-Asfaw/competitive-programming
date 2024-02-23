cadffd1f138a122b906a6856b2a38ba6efaa633b
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2024-02-23 14:44:14 +0300

    Add solution for Reverse String

--- two-pointers and sliding-windows/leetcode/reverse-string.py
+++ two-pointers and sliding-windows/leetcode/reverse-string.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        i=0
-        j=len(s)-1
-        while i<j:
-            s[i],s[j]=s[j],s[i]
-            i+=1
-            j-=1
+        def rec(left,right):
+            if left>=right:
+                return
+            s[left],s[right]=s[right],s[left]
+            return rec(left+1,right-1)
+        rec(0,len(s)-1)
