534380787ef14df9a424217ad5b60b36b7ae8035
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2024-02-27 14:54:42 +0300

    Add solution for Binary Tree Preorder Traversal

---
+++ sliding-windows and two-pointer/leetcode/binary-tree-preorder-traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans=[]
+        def dfs(node):
+            nonlocal ans
+            if node:
+                ans.append(node.val)
+                dfs(node.left)
+                dfs(node.right)
+            return ans
+        return dfs(root)
+        
+        
