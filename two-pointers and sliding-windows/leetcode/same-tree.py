41902bbde9328ec6312124c61650a14b00981536
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2024-02-28 16:40:57 +0300

    Add solution for Same Tree

---
+++ two-pointers and sliding-windows/leetcode/same-tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def dfs(node1,node2):
+            if not node1 and node2:
+                return False
+            elif not node1 and not node2:
+                return True
+            elif node1 and node2:
+                return node1.val==node2.val and dfs(node1.left,node2.left) and dfs(node1.right,node2.right)
+        return dfs(p,q)
